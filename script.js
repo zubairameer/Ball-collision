396c9dc59205b6326860232e31c7bbc710205825
Zubair <113101875+BabushkaBear@users.noreply.github.com>
2024-03-13 14:02:47 +0200

    Circles are more round and added a line feature
    Made the circles more round , used better colours and added a history line to that follows the balls

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight;
 // preparing the animation variables
 const ctx = canvas.getContext('2d');
 const particleArray = [];
-const particleColor = ["#4afc46", "#fcf746","#fc7346", "black", "#fc46f5", "#4687fc"];
+const particleColor = ["#FF000D", "#0165FC","#FFCF00", "#FFFF00", "#FF028D", "#AD0AFD"]; //red, blue, orange, yellow, pink, purple
 // const a = new Audio("/assets/movie_1.mp3");
 
 // Particle Class
@@ -16,11 +16,12 @@ constructor(){
         this.x = canvas.getBoundingClientRect().left + 125; 
         this.y = canvas.getBoundingClientRect().top + 125;
         //sets size of ball
-        this.size = 25;
+        this.size = 50;
         //sets speed of ball
         this.speedx = Math.random() * 20 - 10; //the 20-10 ensures that the  
         this.speedy = Math.random() * 20 - 10; //direction can either be + or - 
         this.color = particleColor[Math.floor(Math.random()*6+1)]; //random generate ini color
+        this.previousPositions = []; //arr for the history line
         // this.sound = a;
      }
      update(){
@@ -43,14 +44,45 @@ constructor(){
                     this.color = particleColor[newColor];
                     // this.sound.play();
          }
+
+          // pushes current position to previous positions
+        this.previousPositions.push({x: this.x, y: this.y});
+
+        // play with limit for the line memory
+        if (this.previousPositions.length > 200) 
+        {
+            this.previousPositions.shift(); // removes the oldest position
+        }
+
      }  
     draw(color){
         //drawing the ball at each call
-         ctx.beginPath();
-         ctx.fillStyle = this.color;
-         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-         ctx.fill();
-         ctx.closePath();
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.linewidth = 100; //adjust thickness of line (abit iffy)
+        for (let k = 1; k < this.previousPositions.length; k++) 
+        {
+            ctx.moveTo(this.previousPositions[k - 1].x, this.previousPositions[k - 1].y);
+            ctx.lineTo(this.previousPositions[k].x, this.previousPositions[k].y);
+        }
+        ctx.stroke();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+    
+        
+        ctx.save();
+        ctx.scale(1, canvas.height / canvas.width);
+    
+        //draw the circle
+        ctx.arc(this.x, this.y / (canvas.height / canvas.width), this.size * 2 , 0, Math.PI * 2, false); //adjust ball size here
+    
+        // restore the context scale 
+        ctx.restore();
+    
+        ctx.fill();
+        ctx.closePath();
      }
  }
 
